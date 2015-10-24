6464fa4efaec0d171013511479307f6395896d4a
kmry <kmr_rtf@live.jp>
2015-10-24 12:29:28 +0900

    add codes

--- src/main/kotlin/sample/HelloController.kt
+++ src/main/kotlin/sample/HelloController.kt
@@ -4,9 +4,24 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
+//追加
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestParam
+
 @Controller
 public class HelloController {
+    @RequestMapping("/send")
+    fun send( model:Model, @RequestParam("name")  name:String) :String{
+      model.addAttribute("name", name)
+      return "hello"    // View file is templates/hello.html
+    }
     @RequestMapping("/hello")
-    @ResponseBody
-    public fun hello() = "Hello world!"
+    fun hello(model: Model) :String{
+      model.addAttribute("name", "直接、URIが入力されました。")
+      return "hello"
+    }
+
+    @RequestMapping("/")
+    fun home() = "index"
+
 }
